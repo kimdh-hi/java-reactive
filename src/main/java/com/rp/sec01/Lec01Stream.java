5f68dfa91bce52fcd622704c62369a4f4ddf53f8
kimdh-hi <zbeld123@gmail.com>
2022-03-07 13:11:50 +0900

    project init & Lazy Stream test

---
+++ src/main/java/com/rp/sec01/Lec01Stream.java
@@ -0,0 +1,31 @@
+package com.rp.sec01;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+public class Lec01Stream {
+
+    public static void main(String[] args) {
+
+        Stream<Integer> stream = Stream.of(1)
+                .map(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return i*2;
+                });
+
+        /**
+         * 결과출력 XX Lazy Stream
+         */
+//        System.out.println(stream);
+
+        /**
+         * 원소출력 OK
+         */
+        stream.forEach(System.out::println);
+    }
+}
